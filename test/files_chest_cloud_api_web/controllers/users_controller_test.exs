9083e32fba3b00d43fd8e58dad3f41fcca714f7f
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-10 13:16:06 -0300

    :test_tube: test: test implementation of the sign_in action.

--- test/files_chest_cloud_api_web/controllers/users_controller_test.exs
+++ test/files_chest_cloud_api_web/controllers/users_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule FilesChestCloudApiWeb.UsersControllerTest do
   use FilesChestCloudApiWeb.ConnCase
 
+  alias FilesChestCloudApi.Accounts
+
   @user_default_params %{
     name: "Guigo",
     email: "guigo.test@example.com",
@@ -35,6 +37,48 @@ defmodule FilesChestCloudApiWeb.UsersControllerTest do
         |> json_response(:bad_request)
 
       assert %{"error" => %{"email" => ["has invalid format"]}} == response
+    end
+  end
+
+  describe "sign_in/2" do
+    test "When credentials are valid, it returns a token.", %{conn: conn} do
+      Accounts.Create.register_user(@user_default_params)
+
+      credentials = %{email: "guigo.test@example.com", password: "supersenha"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, credentials))
+        |> json_response(:ok)
+
+      %{"token" => token} = response
+      assert %{"token" => token} == response
+    end
+
+    test "When the email is invalid, it returns an error message.", %{conn: conn} do
+      Accounts.Create.register_user(@user_default_params)
+
+      credentials = %{email: "wrong_email@example.com", password: "supersenha"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, credentials))
+        |> json_response(:unauthorized)
+
+      assert %{"message" => "Email not registred!"} == response
+    end
+
+    test "When the password is invalid, it returns an error message.", %{conn: conn} do
+      Accounts.Create.register_user(@user_default_params)
+
+      credentials = %{email: "guigo.test@example.com", password: "wrong_password"}
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :sign_in, credentials))
+        |> json_response(:unauthorized)
+
+      assert %{"message" => "Password invalid!"} == response
     end
   end
 end
