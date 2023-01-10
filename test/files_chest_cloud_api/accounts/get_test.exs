bee58a9447929de8bfe92b37c9d82430bc195c2e
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-10 14:41:14 -0300

    :test_tube: test: implementation of get_user_by_id/1 function tests.

---
+++ test/files_chest_cloud_api/accounts/get_test.exs
@@ -0,0 +1,28 @@
+defmodule FilesChestCloudApi.Accounts.GetTest do
+  use FilesChestCloudApi.DataCase
+
+  alias FilesChestCloudApi.Accounts.{User, Create, Get}
+
+  describe "get_user_by_id/1" do
+    test "When the id are valid and user When the id is valid and there is a user with that id, returns the user." do
+      params = %{
+        name: "Guigo",
+        email: "guigo.test@example.com",
+        password: "supersenha"
+      }
+
+      # Get the id of the new user.
+      {:ok, %User{id: user_id}} = Create.register_user(params)
+
+      assert {:ok, %User{id: ^user_id}} = Get.get_user_by_id(user_id)
+    end
+
+    test "When the entered id does not match any user, it returns an error message." do
+      assert {:error, "User does not exists!"} == Get.get_user_by_id("64b66e39-ae33-4d4b-a1ff-08c4c2b5b999")
+    end
+
+    test "When the id format is invalid, it returns an error message." do
+      assert {:error, "Invalid id format!"} == Get.get_user_by_id("invalid_id_format")
+    end
+  end
+end
