06afa168c94980bdb39a385a8cc2818402137053
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-12 19:23:02 -0300

    :test_tube: test: implementation of update_user/ function tests.

---
+++ test/files_chest_cloud_api/accounts/update_test.exs
@@ -0,0 +1,79 @@
+defmodule FilesChestCloudApi.Accounts.UpdateTest do
+  use FilesChestCloudApi.DataCase
+
+  alias FilesChestCloudApi.Accounts.{User, Create, Update}
+
+  @user_default_params %{
+    name: "Guigo",
+    email: "guigo.test@example.com",
+    password: "supersenha"
+  }
+
+  describe "update_user/1" do
+    test "When the parameters are valid and the password is provided, update the user." do
+      {:ok, %User{id: user_id}} = Create.register_user(@user_default_params)
+
+      params_to_update = %{
+        "id" => user_id,
+        "email" => "updated_email@example.com",
+        "password" => "supersenha"
+      }
+
+      response = Update.update_user(params_to_update)
+
+      assert {:ok, %User{email: "updated_email@example.com"}} = response
+    end
+
+    test "When the parameters are valid but the user's password is not supplied, it returns an error message." do
+      {:ok, %User{id: user_id}} = Create.register_user(@user_default_params)
+
+      params_to_update = %{
+        "id" => user_id,
+        "email" => "updated_email@example.com",
+        "password" => ""
+      }
+
+      {:error, invalid_changeset} = Update.update_user(params_to_update)
+
+      assert errors_on(invalid_changeset) == %{password: ["can't be blank"]}
+    end
+
+    test "When any parameter is invalid, it returns an error message." do
+      {:ok, %User{id: user_id}} = Create.register_user(@user_default_params)
+
+      params_to_update = %{
+        "id" => user_id,
+        "email" => "",
+        "password" => "supersenha"
+      }
+
+      {:error, invalid_changeset} = Update.update_user(params_to_update)
+
+      assert errors_on(invalid_changeset) == %{email: ["can't be blank"]}
+    end
+
+    test "When the provided is not assigned to any user, it returns an error message." do
+      params_to_update = %{
+        "id" => "4e27ecb5-cb0b-4674-99cd-522656aec893",
+        "email" => "updated_email@example.com",
+        "password" => "supersenha"
+      }
+
+      response = Update.update_user(params_to_update)
+
+      assert {:error, "User does not exists!"} == response
+    end
+
+    test "When the provided id is invalid, it returns an error message." do
+      params_to_update = %{
+        "id" => "invalid_id_format",
+        "email" => "updated_email@example.com",
+        "password" => "supersenha"
+      }
+
+      response = Update.update_user(params_to_update)
+
+      assert {:error, "Invalid id format!"} == response
+    end
+  end
+end
