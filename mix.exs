65b3b017aa692123a2eb9dfb782cd173621748c2
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-22 17:16:56 -0300

    :rocket: deploy: set elixir and erlang version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FilesChestCloudApi.MixProject do
     [
       app: :files_chest_cloud_api,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
